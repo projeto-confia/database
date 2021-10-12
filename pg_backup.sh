20270e1cc63b45925dfcea6ac89782264dda28a5
Gabriel Machado <gabriel.rmachado10@gmail.com>
2021-10-12 18:35:48 -0300

    Alteraçoes do script de criaçao: (i) remoçao das stored procedures; (ii) criaçao da coluna 'is_reliable' na tabela 'owner'; (iii) inclusao dos comandos de backup (comentados) em 'pg_backup.sh' para backup apenas do schema e backup apenas dos dados.

--- pg_backup.sh
+++ pg_backup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# data and schema
 docker exec -t confia-db pg_dump -d confia -U admin | gzip > pg_backup/dump_$(date +"%Y-%m-%d_%H_%M_%S").sql.gz
+
+# only schema
+# docker exec -t confia-db pg_dump -d confia -U admin --schema-only | gzip > pg_backup/dump_$(date +"%Y-%m-%d_%H_%M_%S").sql.gz
+
+# only data
+# docker exec -t confia-db pg_dump -d confia -U admin --data-only | gzip > pg_backup/dump_$(date +"%Y-%m-%d_%H_%M_%S").sql.gz
